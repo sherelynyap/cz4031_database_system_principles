0db2921a073b88138de90895d8eed6b3666539df
LEONG HONG YI <100272517+HLeong12345@users.noreply.github.com>
2023-09-17 18:02:07 +0800

    Refactored Node, LeafNode, InternalNode

--- BPlusTree/LeafNode.java
+++ BPlusTree/LeafNode.java
@@ -3,78 +3,92 @@ package BPlusTree;
 import Storage.Address;
 import java.util.ArrayList;
 
-public class LeafNode extends Node{
-    private ArrayList<Address> addresses;
-    private LeafNode nextNode;
+public class LeafNode extends Node {
+    private ArrayList<Address> addressesSet;
+    private LeafNode nextLeafNode;
 
-    public LeafNode(){
+    public LeafNode() {
+        // Refactored
         super();
-        setIsRootNode(false);
-        setIsLeafNode(true);
-        addresses = new ArrayList<Address>();
-        setNextNode(null);
+        super.setIsRootNode(false);
+        super.setIsLeafNode(true);
+        this.addressesSet = new ArrayList<Address>();
+        this.nextLeafNode = null;
     }
 
     public ArrayList<Address> getAddresses() {
-        return addresses;
+        return this.addressesSet;
     }
 
-    public Address getAddress(int i) {
-        return addresses.get(i);
+    public Address getAddress(int targetIndex) {
+        Address targetAddress = addressesSet.get(targetIndex);
+        return targetAddress;
     }
 
     /**
-    SetAddress(int key, Address address): sets an address for a given key in a leaf node (current node).
-
-    If the node is empty:
-        sets the key and address at index 0
-    Else, it uses the setKey method from the Node class to set the key at the appropriate index and
-    adds the address to the addresses list at the same index.
-    The method then performs a for loop to shift all the subsequent addresses to the right to make room for
-    the new address at the given index.
-    Finally, it sets the index and address in the addresses list of the current node.
-    Returns the index at which the key was inserted.
+     * SetAddress(int key, Address address): sets an address for a given key in a
+     * leaf node (current node).
+     * 
+     * If the node is empty:
+     * sets the key and address at index 0
+     * Else, it uses the setKey method from the Node class to set the key at the
+     * appropriate index and
+     * adds the address to the addresses list at the same index.
+     * The method then performs a for loop to shift all the subsequent addresses to
+     * the right to make room for
+     * the new address at the given index.
+     * Finally, it sets the index and address in the addresses list of the current
+     * node.
+     * Returns the index at which the key was inserted.
      */
-    public int setAddress(int key, Address address){
-        if (this.getAddresses().size() == 0) {
-            this.setKey(key);
-            this.addresses.add(address);
-            return 0;
+    public int setAddress(int targetKey, Address targetAddress) {
+        int addressSetSize = this.getAddresses().size();
+        int targetIndex = 0;
+        if (addressSetSize == 0) {
+            this.setKey(targetKey);
+            this.addressesSet.add(targetAddress);
+            // return 0;
+        } else {
+            targetIndex = super.setKey(targetKey);
+            addressesSet.add(targetAddress);
+            addressSetSize = this.getAddresses().size();
+
+            for (int ptr = addressSetSize - 2; ptr >= targetIndex; ptr--)
+                addressesSet.set(ptr + 1, addressesSet.get(ptr));
+            addressesSet.set(targetIndex, targetAddress);
+            // return targetIndex;
         }
-        int index = super.setKey(key);
-        addresses.add(address);
-
-        for (int i = addresses.size() -2; i >= index; i--)
-            addresses.set(i+1, addresses.get(i));
-
-        addresses.set(index, address);
-        return index;
+        return targetIndex;
     }
 
-
-    public void deleteAddress(int i) {
-        doKeyDeletion(i);
-        addresses.remove(i);
+    public void deleteAddress(int targetIndex) {
+        doKeyDeletion(targetIndex);
+        addressesSet.remove(targetIndex);
     }
 
     public void deleteAddresses() {
-        addresses = new ArrayList<Address>();
+        // addresses = new ArrayList<Address>();
+        resetAddresses();
     }
 
     public LeafNode getNextNode() {
-        return nextNode;
+        return this.nextLeafNode;
     }
 
-    public void setNextNode(LeafNode nextNode) {
-        this.nextNode = nextNode;
+    public void setNextNode(LeafNode updateNextLeafNode) {
+        this.nextLeafNode = updateNextLeafNode;
     }
 
     /**
-    doSeparation(): Deletes all keys and resets all the addresses
-    */
-    public void doSeparation(){
+     * doSeparation(): Deletes all keys and resets all the addresses
+     */
+    public void doSeparation() {
         doKeysDeletion();
-        addresses = new ArrayList<Address>();
+        resetAddresses();
+    }
+
+    public void resetAddresses() {
+        this.addressesSet = new ArrayList<Address>();
     }
 
 }
